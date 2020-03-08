883141c8b01eb0c30edccde9273122c2ec564f96
Deniz Gökçin <dgokcin@gmail.com>
2020-03-08 20:25:41 +0300

    add more linear filters

--- shift-left-demo.py
+++ shift-left-demo.py
@@ -3,19 +3,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Replaces each pixel with an average of its neighborhood
-
 img = cv2.imread('pics/opencv_logo.png')
 
 # smoothed = cv2.boxFilter(img, 0, (9, 9), img, (1, 1))
-kernel = np.array([[1, 1, 1],
-                   [1, 1, 1],
-                   [1, 1, 1]])/9
+kernel = np.array([[0, 0, 0],
+                   [0, 0, 1],
+                   [0, 0, 0]])
 
 # applying the sharpening kernel to the input image & displaying it.
-smoothed = cv2.filter2D(img, -1, kernel)
+shifted = cv2.filter2D(img, -1, kernel)
 
 plt.subplot(121), plt.imshow(img), plt.title('original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(smoothed), plt.title('smoothed')
+plt.subplot(122), plt.imshow(shifted), plt.title('shifted left')
 plt.xticks([]), plt.yticks([])
 plt.show()
--- sobel-horizontal-filter-demo.py
+++ sobel-horizontal-filter-demo.py
@@ -8,17 +8,15 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('pics/einstein.png')
 
-kernel = np.array([[1, 1, 1],
-                   [1, 1, 1],
-                   [1, 1, 1]])/9
+kernel = np.array([[1, 2, 1],
+                   [0, 0, 0],
+                   [-1, -2, -1]])
 
 # applying the sharpening kernel to the input image & displaying it.
-smoothed = cv2.filter2D(img, -1, kernel)
-sharpened = cv2.addWeighted(img, 2, smoothed, -1, 0)
+sobeled = cv2.filter2D(img, -1, kernel)
 
 plt.subplot(121), plt.imshow(img), plt.title('original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(sharpened), plt.title('sharpened')
+plt.subplot(122), plt.imshow(sobeled), plt.title('sobel horizontal filter')
 plt.xticks([]), plt.yticks([])
 plt.show()
-
